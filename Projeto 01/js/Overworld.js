3e92fcfe5b82fa2cc0b9c6c45edc45ac17fbb3d7
João Pedro <j0310pedro@gmail.com>
2022-01-14 20:42:27 -0300

    a

--- Projeto 01/js/Overworld.js
+++ Projeto 01/js/Overworld.js
@@ -47,7 +47,7 @@ class Overworld {
     }
 
     init() {
-        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen);
 
         // chama o DirectinoInput.js
         this.directionInput = new DirectionInput();
--- Projeto 01/js/OverworldMap.js
+++ Projeto 01/js/OverworldMap.js
@@ -25,40 +25,26 @@ class OverworldMap {
 }
 
 window.OverworldMaps = {
-    DemoRoom: {
+    LivingRoom: {
         lowerSrc: "imagens/maps/DemoLower.png",
-        upperSrc: "imagens/maps/DemoUpper.png",
+        upperSrc: "imagens/maps/LivingRoom.png",
         gameObjects: {
             hero: new Person({
                 isPlayerControlled: true,
                 x: utils.withGrid(4),
                 y: utils.withGrid(6),
             }),
-            npc1: new Person({
-                x: utils.withGrid(7),
-                y: utils.withGrid(9),
-                src: "imagens/characters/people/npc1.png"
-            })
         }
     },
     Kitchen: {
         lowerSrc: "imagens/maps/KitchenLower.png",
         upperSrc: "imagens/maps/KitchenUpper.png",
         gameObjects: {
-            hero: new GameObject({
-                x: 5,
-                y: 6,
+            hero: new Person({
+                isPlayerControlled: true,
+                x: utils.withGrid(4),
+                y: utils.withGrid(6),
             }),
-            npc2: new GameObject({
-                x: 9,
-                y: 6,
-                src: "imagens/characters/people/npc2.png"
-            }),
-            npc3: new GameObject({
-                x: 10,
-                y: 8,
-                src: "imagens/characters/people/npc3.png"
-            })
         }
     },
 }
